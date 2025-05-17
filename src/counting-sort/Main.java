2f341f5e8a972498d2c0b3ba53d8b9ff84d9a049
noprizal <rizalnov667@gmail.com>
2025-05-17 21:33:33 +0700

    fixing

--- src/counting-sort/Main.java
+++ src/counting-sort/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = {6, 3, 8, 5, 2, 7, 4, 1};
+        int[] array = {6, 3, 8, 5, 2, 7};
 
         System.out.println("Sebelum diurutkan:");
         printArray(array);
